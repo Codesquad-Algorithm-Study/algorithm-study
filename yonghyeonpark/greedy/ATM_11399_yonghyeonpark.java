8ecb1b4046e0b27533d3e67643bdf9d42c50429f
yonghyeonpark <pyh4616@naver.com>
2024-07-19 03:56:52 +0900

    ATM, 132ms, 14396KB

---
+++ yonghyeonpark/greedy/ATM_11399_yonghyeonpark.java
@@ -0,0 +1,30 @@
+package yonghyeonpark.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ATM_11399_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int accum = 0;
+        int result = 0;
+        for (int element : arr) {
+            accum += element;
+            result += accum;
+        }
+        System.out.println(result);
+    }
+}
