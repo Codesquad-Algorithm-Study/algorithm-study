ce7313b6eb881f5820a46e6d4b6410a4b60f9a00
sharpie1330 <amberaplin14@gmail.com>
2024-07-21 01:34:02 +0900

    숫자 0, 188ms, 17728KB

---
+++ sharpie1330/greedy/숫자0_11047_sharpie1330.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.*;
+
+public class 숫자0_11047_sharpie1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] V = new int[N];
+        for (int i = 0; i < N; i++) {
+            V[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (K / V[i] == 0) {
+                continue;
+            }
+            answer += K / V[i];
+            K = K % V[i];
+        }
+
+        System.out.println(answer);
+    }
+}
