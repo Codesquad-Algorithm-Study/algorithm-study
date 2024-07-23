bd5afd79b0beb9815ae1d684aafd09f3cb3b8467
yonghyeonpark <pyh4616@naver.com>
2024-07-24 03:10:54 +0900

    카드, 448ms, 36968KB

---
+++ yonghyeonpark/sort/카드_11652_yonghyeonpark.java
@@ -0,0 +1,30 @@
+package yonghyeonpark.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 카드_11652_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] cards = new long[n];
+        for (int i = 0; i < n; i++) {
+            cards[i] = Long.parseLong(br.readLine());
+        }
+
+        Map<Long, Long> map = new HashMap<>();
+        for (long card : cards) {
+            map.put(card, map.getOrDefault(card, 0L) + 1);
+        }
+
+        List<Map.Entry<Long, Long>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(Comparator.comparingLong(Map.Entry<Long, Long>::getValue).reversed()
+                .thenComparingLong(Map.Entry::getKey));
+
+        System.out.println(entryList.get(0).getKey());
+    }
+}
